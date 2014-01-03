4612e3cf4dfaf0497d1e559b29fe5eeb3af040ab
ChungYeul In <sldk86@naver.com>
2014-01-03 18:12:33 +0900

    107페이지 실습 내용을 추가 하였습니다.
    107페이지 실습 내용을 추가 하였습니다.

--- TextFieldTest/ViewController.m
+++ TextFieldTest/ViewController.m
@@ -8,12 +8,36 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 
 @end
 
 @implementation ViewController
+// 편집 상태 승인 여부
+-(BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    if (2 == textField.tag) {
+        return NO;
+    }
+    return YES;
+}
+
+// 리턴키로 키보드 감추기
+-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+    NSLog(@"INPUT : %@", textField.text);
+    [textField resignFirstResponder];
+    return YES;
+}
+
+// z키 입력 불가
+-(BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    // 대소문자 구분없이 비교
+    if (3 == textField.tag && NSOrderedSame == [string compare:@"z" options:NSCaseInsensitiveSearch]) {
+        return NO;
+    }
+    return YES;
+}
+
 - (IBAction)dismissKeyboard:(id)sender {
     [self.textField resignFirstResponder];
 }
@@ -25,7 +49,22 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // 레이블
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20, 280, 0, 0)];
+    label.text = @"Z 입력 금지";
+    label.textColor = [UIColor whiteColor];
+    label.backgroundColor = [UIColor clearColor];
+    [label sizeToFit];
+    [self.view addSubview:label];
+    
+    // 텍스트 필드 객체 생성
+    UITextField *textField = [[UITextField alloc] initWithFrame:CGRectMake(130, 276, 170, 30)];
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    textField.delegate = self;
+    textField.tag = 3;
+    
+    // 뷰 구조에 추가
+    [self.view addSubview:textField];
 }
 
 - (void)didReceiveMemoryWarning
